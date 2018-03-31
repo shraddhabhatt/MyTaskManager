ce38ab21bcc933c315ff590515ed03b29a6a225d
shraddhabhatt <trivedishraddha99@gmail.com>
2018-03-30 21:47:54 -0400

    inital basic structure code commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# MyTaskManager
+# My Task Manager
+  
---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+### Schema
+CREATE DATABASE dbname;
+USE dbname;
+
+CREATE TABLE tablename
+(
+	id int NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (id)
+);
+
+
