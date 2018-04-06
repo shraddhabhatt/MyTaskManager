9876fd97f8904299bae1d3a765d5d5185265caba
clarkwmcd <clarkwmcd@gmail.com>
2018-04-06 17:07:54 -0400

    added twilio functionality

---
+++ keys.js
@@ -0,0 +1,7 @@
+
+require("exports"); //download the exports npm package
+
+exports.twilio = {
+  accountSid: process.env.TWILIO_ACCOUNTSID,
+  authToken: process.env.TWILIO_AUTHTOKEN
+}
--- models/user.js
+++ models/user.js
@@ -22,6 +22,13 @@ module.exports = function(sequelize, DataTypes) {
       validate: {
         len: [1]
       }
+    },
+    phonenumber: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      validate: {
+        len: [1]
+      }
     }
   });
 
